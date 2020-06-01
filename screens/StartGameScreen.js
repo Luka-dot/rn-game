26c40634c2715274052cefc5c97389fcefc90871
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-05-31 20:42:33 -0400

    set up start screen component on app.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,10 +3,19 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const StartGameScreen = props => {
 
+    return (
+        <View style={styles.screen}>
+            <Text>test start screen</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    }
 });
 
 export default StartGameScreen;
