5687d113ad7390240999dcbcf6adc8e13bb55ee1
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-05-31 21:09:00 -0400

    start game screen styling added

---
+++ components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Card = props => {
+    return <View style={{...styles.card}}>{props.children}</View>
+},
+
+const styles = StyleSheet.create({
+    card: {    
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 11,
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 10
+    }
+});
+
+export default Card;
