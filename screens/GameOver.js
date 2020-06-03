728f5270c66038062c618500cd0a736567cf831d
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-06-02 21:27:28 -0400

    images added and styled

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
-import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText'
@@ -11,7 +10,13 @@ const GameOverScreen = props => {
     return <View style={styles.main} > 
             <Card style={styles.screen} >
                 <TitleText>Game is over</TitleText>
-                <Image source={require('../assets/success.png')} />
+                <View style={styles.imageContainer}>
+                    <Image 
+                        source={require('../assets/success.png')} 
+                        style={styles.image} 
+                        resizeMode="cover"
+                    />
+                </View>
                 <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
                 <BodyText>Picked number was: {props.userNumber}</BodyText>
                 <Button title="NewGame" onPress={props.onRestart} />
@@ -30,6 +35,19 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 300,
         maxWidth: '80%'
+    },
+    imageContainer: {
+        width: '80%',
+        height: 300,
+        borderRadius: 20,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        margin: 20
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
