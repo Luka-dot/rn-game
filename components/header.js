1992e6dc131d0cea4d387a8cc24517c6fb18dbca
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-06-04 08:47:25 -0400

    Platform API to check for operation system added

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 
 import Colors from '../constants/Colors';
 import TitleText from '../components/TitleText';
@@ -7,7 +7,7 @@ import TitleText from '../components/TitleText';
 const Header = props => {
     return (
         <View style={styles.header}>
-            <TitleText>{props.title}</TitleText>
+            <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     )
 };
@@ -17,9 +17,15 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
+        // setting background wit use of Platform. diff for each system
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+    },
+    title: {
+        color: Platform.OS === 'ios' ? Colors.primary : 'white', 
     }
 });
 
